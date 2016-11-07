7965263035ce86e8b7c21761de3a2db3ce444855
natewilliams1 <nwilliams@daxko.com>
2016-11-07 11:40:52 -0600

    block_member_spec

--- spec/block_member_spec.rb
+++ spec/block_member_spec.rb
@@ -1,48 +1,43 @@
-# # This is still a work in progress
-# # I am stuck on actually clicking the block button.
-# # I am not sure how to select a dt.
-# # Also, the block feature is currently broken.
-# # That means that I don't know what it should actually do :)
-#
-# describe 'Check In' do
-#   context 'A valid member' do
-#     before(:all) do
-#       @user_name = 'test_automation'
-#       @user_password = 'UP83uN7pM5sEmBfA!'
-#
-#       visit SignInPage do |page|
-#         page.login(
-#             user_name: @user_name,
-#             user_password: @user_password
-#         )
-#       end
-#
-#       on CheckInPage do |page|
-#         page.search_field = '120359-04'
-#         page.go_button
-#         page.member_table_element.when_present
-#         sleep 5
-#         page.gear_icon_element.click
-#         # sleep 5
-#         # page.block_member_element.when_present
-#         # sleep 5
-#         page.block_member_element.click
-#         sleep 5
-#       end
-#
-#     end
-#
-#     after(:all) do
-#       on CheckInPage do |page|
-#         page.sign_out
-#       end
-#     end
-#
-#     it 'verifies checkin success message is present' do
-#       on CheckInPage do |page|
-#         page.checkin_success_element.when_present
-#         expect(page.checkin_success_element.text).to include("visits this month")
-#       end
-#     end
-#   end
-# end
+
+
+describe 'Block' do
+  context 'A visiting member' do
+    before(:all) do
+      @user_name = 'test_automation'
+      @user_password = 'UP83uN7pM5sEmBfA!'
+
+      visit SignInPage do |page|
+        page.login(
+            user_name: @user_name,
+            user_password: @user_password
+        )
+      end
+
+      on CheckInPage do |page|
+        page.search_field = '120359-04'
+        page.go_button
+        page.member_table_element.when_present(timeout = 60)
+        page.gear_icon_element.when_present.click
+        page.block_member_element.when_present.click
+      end
+
+    end
+
+    after(:all) do
+      on CheckInPage do |page|
+        page.gear_icon_element.when_present.click
+        sleep 1
+        page.unblock_member_element.when_present.click
+        sleep 1
+        page.sign_out
+      end
+    end
+
+    it 'verifies that visiting member is successfully blocked' do
+      on CheckInPage do |page|
+        page.primary_action_cell_element.when_present
+        expect(page.primary_action_cell_element.text).to include("Blocked")
+      end
+    end
+  end
+end
